db1335c12144c0deac95651fb2a73322ad9b8cca
Marcel Lis <lis.marcys@gmail.com>
2021-05-12 14:03:10 +0200

    Builded first working web API

---
+++ src/server/.net/FoxSky.TeachApp.WebApi/Controllers/AdministrationController.cs
@@ -0,0 +1,44 @@
+﻿using FoxSky.TeachApp.Service;
+using FoxSky.TeachApp.Service.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxSky.TeachApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("webapi/administration")]
+    public class AdministrationController : ControllerBase
+    {
+        [HttpGet]
+        [Route("addtestuser")]
+        public int PutUsers()
+        {
+            return new UserService().AddUser(new UserData { Forename = "linux", Surname = "debian"});
+        }
+
+        [HttpPost]
+        [Route("adduser")]
+        public int AddUser([FromForm] UserData userData)
+        {
+            return new UserService().AddUser(userData);
+        }
+
+        [HttpGet]
+        [Route("users")]
+        public IList<UserData> GetUsers()
+        {
+            return new UserService().GetUsers();
+        }
+
+        [HttpPost]
+        [Route("delete")]
+        public void DeleteUser([FromForm] int id)
+        {
+            new UserService().DeleteUser(id);
+        }
+    }
+}
